627d55b2fc8b67e42d7901b01b829156dfd30c09
cawilson1 <cawilson1@g.cofc.edu>
2021-06-18 08:39:42 -0400

    factory uses docClient and dynamodb

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
+const { mergeDeepRight, defaultTo, pipe, ifElse, identity } = require("ramda");
+const AWS = require("aws-sdk");
 
-const { mergeDeepRight, defaultTo, pipe, ifElse, identity } = require('ramda')
-const AWS = require('aws-sdk')
-
-const createAdapter = require('./adapter')
-
+const createAdapter = require("./adapter");
 
 /**
-  * @typedef {Object} DynamoAdapterPluginConfig
-  * @property {string} apiVersion - the aws api version to use
-  * @property {string} region - the aws region to use
+ * @typedef {Object} DynamoAdapterPluginConfig
+ * @property {string} apiVersion - the aws api version to use
+ * @property {string} region - the aws region to use
  */
 
 /**
  * @param {DynamoAdapterPluginConfig} config
  * @returns {object}
  */
-module.exports = function DynamoDataAdapter (config) {
+module.exports = function DynamoDataAdapter(config) {
+  const { accessKeyId, secretAccessKey, region } = config;
+
   return Object.freeze({
-    id: 'dynamodb-data-adapter',
-    port: 'data',
+    id: "dynamodb-data-adapter",
+    port: "data",
     /**
      * The load function that returns the config object
      * that is then passed to the link function. For now, just a passthrough
@@ -29,22 +29,34 @@ module.exports = function DynamoDataAdapter (config) {
       defaultTo({}),
       mergeDeepRight(config),
       ifElse(
-        config => config.region && config.apiVersion,
+        config => region && accessKeyId && secretAccessKey,
         identity,
-        () => { throw new Error('Config region and apiVersion must be provided') }
+        () => {
+          throw new Error(
+            "Config region and access key id and access secret must be provided"
+          );
+        }
       )
     ),
     /**
      * The link function that returns the adapter instance
      *
-     * @param {DynamoAdapterPluginConfig} env 
+     * @param {DynamoAdapterPluginConfig} env
      * @returns the adapter
      */
     link: env => _ => {
-      AWS.config.update(env.region)
-      const ddb = new AWS.DynamoDB({ apiVersion: env.apiVersion })
-
-      return createAdapter({ ddb })
+      const docClient = new AWS.DynamoDB.DocumentClient({
+        accessKeyId,
+        secretAccessKey,
+        region
+      });
+      const dynamoDb = new AWS.DynamoDB({
+        accessKeyId,
+        secretAccessKey,
+        region
+      });
+      const ddb = { docClient, dynamoDb };
+      return createAdapter({ ddb });
     }
-  })
-}
+  });
+};
